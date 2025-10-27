a8066ce87f97c91d7cbc6406b00084d5b8b16317
Gustavo Adolfo Olmedo <gustavo.olmedo@mindera.com>
2025-10-27 21:49:41 +0000

    add menu spec

---
+++ tests/menu.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("Menu HOME validation", async ({ page }) => {
+  await page.goto("https://playground-drab-six.vercel.app/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playground page/);
+
+  await page.getByRole("link", { name: "LOGIN" }).click();
+  // await page.getByRole("link", { name: "FORM" }).click();
+  // await page.getByRole("link", { name: "TABLE" }).click();
+  // await page.getByRole("link", { name: "TASKS" }).click();
+  // await page.getByRole("link", { name: "STORE" }).click();
+  // await page.getByRole("link", { name: "ABOUT" }).click();
+
+  await expect(page.getByRole("heading", { name: "LOGIN" })).toBeVisible();
+});
