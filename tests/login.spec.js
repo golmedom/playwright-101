5b05d374753de69b9924fa01486714dd29de240d
Gustavo Adolfo Olmedo <gustavo.olmedo@mindera.com>
2025-10-29 21:27:05 +0000

    update login spec with invalid user test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -24,3 +24,16 @@ test("Blocked account", async ({ page }) => {
   await page.getByRole("button", { name: "Login" }).click();
   await expect(page.getByText("User blocked!")).toBeVisible();
 });
+
+test("Invalid user (User not found!)", async ({ page }) => {
+  await page.goto("/login");
+
+  await page
+    .getByRole("textbox", { name: "Type your username" })
+    .fill("test-not-found");
+  await page
+    .getByRole("textbox", { name: "Type your password" })
+    .fill("password123-not-found");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByText("User not found!")).toBeVisible();
+});
