127e959611916a9d13ee33a1a7ea33e7043fadac
Gustavo Adolfo Olmedo <gustavo.olmedo@mindera.com>
2025-10-29 21:26:09 +0000

    add login spec with login successfully test

---
+++ tests/login.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("Login successfully", async ({ page }) => {
+  await page.goto("/login");
+
+  await page.getByRole("textbox", { name: "Type your username" }).fill("test");
+  await page
+    .getByRole("textbox", { name: "Type your password" })
+    .fill("password123");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByText("User successfully logged in!")).toBeVisible();
+});
