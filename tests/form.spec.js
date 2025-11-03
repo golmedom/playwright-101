918414a5df374c443b16b7975803892d25586cfd
Gustavo Adolfo Olmedo <gustavo.olmedo@mindera.com>
2025-11-03 21:54:48 +0000

    add tests for form submission

---
+++ tests/form.spec.js
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+import { USERS } from "./data/form";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/form");
+});
+
+test.describe("Form submission tests", () => {
+  for (const user of USERS) {
+    test(`Submit form scenario: ${user.scenario}`, async ({ page }) => {
+      await page.fill("#name", user.name);
+      await page.fill("#email", user.email);
+      await page.fill("#password", user.password);
+
+      await page.selectOption("#country", user.countryValue);
+
+      await page
+        .locator(`input[type="radio"][value="${user.genderValue}"]`) // I had to do it with this locator otherwise fails, this works like css
+        .check();
+
+      const allHobbyBoxes = page.locator(
+        '#hobbiesGroup input[type="checkbox"]'
+      );
+
+      const count = await allHobbyBoxes.count();
+      for (let i = 0; i < count; i++) {
+        const box = allHobbyBoxes.nth(i); // this works like css
+        if (await box.isChecked()) await box.uncheck();
+      }
+
+      for (const hobby of user.hobbies) {
+        await page.getByRole("checkbox", { name: hobby }).check();
+      }
+
+      await page.click("#submitBtn");
+
+      await expect(page).toHaveURL(/submitted|success|result/i);
+      await expect(page.getByText("Success!")).toBeVisible();
+      await expect(page.getByText("The form has been submitted")).toBeVisible();
+    });
+  }
+});
